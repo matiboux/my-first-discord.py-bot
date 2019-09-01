43a0ad5ad2c060591d30595c43796bf58475a688
Matiboux <matiboux@gmail.com>
2019-09-01 20:37:59 +0200

    Added bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+import logging
+
+# Parameters
+PREFIX = "!"
+
+# Logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(PREFIX + 'hello'):
+        await message.channel.send('Hello, {0}!'.format(message.author.mention))
+
+# client.run('token')
